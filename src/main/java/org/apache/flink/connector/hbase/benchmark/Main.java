3b6b63d54775882dab682ef40a694ea2aa21bd7f
Leon Bein <leon.bein@student.hpi.de>
2021-02-25 17:27:07 +0100

    Add createData

--- src/main/java/org/apache/flink/connector/hbase/benchmark/Main.java
+++ src/main/java/org/apache/flink/connector/hbase/benchmark/Main.java
@@ -1,9 +1,19 @@
 package org.apache.flink.connector.hbase.benchmark;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.TableDescriptor;
+import org.apache.hadoop.hbase.replication.ReplicationPeerDescription;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
+    public static final Configuration HBASE_CONFIG = getDefaultConfig();
 
     public static void main(String[] args) {
         for (RunConfig runConfig : allRunConfigurations()) {
@@ -23,9 +33,6 @@ public class Main {
             this.goal = goal;
             this.target = target;
         }
-
-        public void run() {
-        }
     }
 
     public static class Run {
@@ -37,22 +44,46 @@ public class Main {
         }
 
         public void run() {
-            clearReplications();
+            clearReplicationPeers();
             clearTables();
-            tableNames = config.target.createTables();
             setupFlinkEnvironment();
+            createData();
             waitForTermination();
             retrieveResults();
         }
 
 
-        private void clearReplications() {
+        private void clearReplicationPeers() {
+            System.out.println("Clearing replication peers ...");
+            try (Admin admin = ConnectionFactory.createConnection(HBASE_CONFIG).getAdmin()) {
+                for (ReplicationPeerDescription desc : admin.listReplicationPeers()) {
+                    admin.removeReplicationPeer(desc.getPeerId());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         private void clearTables() {
+            System.out.println("Clearing tables ...");
+            try (Admin admin = ConnectionFactory.createConnection(HBASE_CONFIG).getAdmin()) {
+                for (TableDescriptor desc : admin.listTableDescriptors()) {
+                    admin.deleteTable(desc.getTableName());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void createTables() {
+            tableNames = config.target.createTables(config);
         }
 
         private void setupFlinkEnvironment() {
+        }
+
+        private void createData() {
+
         }
 
         private void waitForTermination() {
@@ -70,17 +101,17 @@ public class Main {
     }
 
     public static abstract class BenchmarkTarget {
-        public abstract String[] createTables();
+        public abstract String[] createTables(RunConfig config);
 
         public static class Source extends BenchmarkTarget {
             @Override
-            public String[] createTables() {
+            public String[] createTables(RunConfig config) {
                 return null;
             }
         }
         public static class Sink extends BenchmarkTarget {
             @Override
-            public String[] createTables() {
+            public String[] createTables(RunConfig config) {
                 return null;
             }
         }
@@ -99,5 +130,17 @@ public class Main {
             }
         }
         return configs;
+    }
+
+
+    private static Configuration getDefaultConfig() {
+        Configuration configuration = HBaseConfiguration.create();
+
+        configuration.setInt("replication.stats.thread.period.seconds", 5);
+        configuration.setLong("replication.sleep.before.failover", 2000);
+        configuration.setInt("replication.source.maxretriesmultiplier", 10);
+        configuration.setBoolean("hbase.replication", true);
+
+        return configuration;
     }
 }
